944ff79415f0a80920f0bcbbe2365af4684dbcbb
shoaib-21 <84820849+shoaib-21@users.noreply.github.com>
2021-12-18 20:11:45 +0530

    Add files via upload

---
+++ apis 2/get.js
@@ -0,0 +1,14 @@
+// https://www.virustotal.com/api/v3/{collection name}/{object id}?relationships={relationship}
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     Accept: 'application/json',
+//     'x-apikey': '6d96efe421861509b0b7ec99c33a98a7671b0b0d2c3af7d4f31eb31256796502'
+//   }
+// };
+
+// fetch('https://www.virustotal.com/api/v3/analyses/mnbmn/bbnbnb', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
